d8644b86de4ce9cc6c825103206fbe3ce7c99eec
YUN HUI JAE <145467592+younheejae@users.noreply.github.com>
2026-07-28 14:16:22 +0900

    solve: 추억 점수.java

---
+++ Programmers/LV1/추억 점수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for(int i=0; i<photo.length; i++) {
+            int score = 0;
+            for(String person: photo[i]) {
+                if(map.containsKey(person)) {
+                    score += map.get(person);
+                }
+            }
+            answer[i] = score;
+        }
+        
+        return answer;
+    }
+}
