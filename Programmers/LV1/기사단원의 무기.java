e179fdf91299bddd3bbc8f3aece9f99b4f787a74
YUN HUI JAE <145467592+younheejae@users.noreply.github.com>
2026-07-28 14:18:06 +0900

    solve: 기사단원의 무기.java

---
+++ Programmers/LV1/기사단원의 무기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+                
+        for(int i=1; i<=number; i++) {
+            int count = 0;
+            
+            for(int j=1; j * j <= i; j++) {
+                if(i % j == 0) { 
+                    count++;
+                    
+                    if(j != i/j) count++;
+                }
+            }
+                        
+            if(count <=limit) {
+                answer += count;
+            } else {
+                answer += power;
+            }
+        }
+            
+        return answer;
+    }
+}
